a23df4ff8d847131376689a3ee42c7bf6372637f
vrittech-manoj <manoj@vrittechnologies.com>
2024-03-21 18:04:29 +0545

    cart to order ongoing

--- order/viewsets/order_viewsets.py
+++ order/viewsets/order_viewsets.py
@@ -1,6 +1,9 @@
 from ..models import Order
 from ..serializers.order_serializer import OrderReadSerializers,OrderWriteSerializers
 from ..utilities.importbase import *
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
 
 class OrderViewsets(viewsets.ModelViewSet):
     serializer_class = OrderReadSerializers
@@ -14,3 +17,6 @@ class OrderViewsets(viewsets.ModelViewSet):
             return OrderWriteSerializers
         return super().get_serializer_class()
     
+    @action(detail=False, methods=['post'], name="cart_to_order", url_path="cart-to-order")
+    def cart_to_order(self, request):
+        return Response({"message": "add to order sucessfully."}, status=status.HTTP_201_CREATED)
