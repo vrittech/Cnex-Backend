377a95f1fab9896afcdf2cd7e7325a7a89bc2df5
vrittech-manoj <manoj@vrittechnologies.com>
2024-03-27 14:14:05 +0545

    appoint swagger checkout

--- appointment/viewsets/checkout_appointment_viewsets.py
+++ appointment/viewsets/checkout_appointment_viewsets.py
@@ -4,10 +4,13 @@ from ..utilities.importbase import *
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 class CheckoutAppointmentViewsets(viewsets.ModelViewSet):
     serializer_class = CheckoutAppointmentReadSerializers
-    permission_classes = [AdminViewSetsPermission]
+    permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
     pagination_class = MyPageNumberPagination
     queryset  = CheckoutAppointment.objects.all()
@@ -25,13 +28,15 @@ class CheckoutAppointmentViewsets(viewsets.ModelViewSet):
                 'slots': openapi.Schema(type=openapi.TYPE_INTEGER),
                 'appointment_date':openapi.Schema(type=openapi.TYPE_STRING),
                 'user':openapi.Schema(type=openapi.TYPE_INTEGER)
-
             },
             required=['service', 'slots','appointment_date','user']
         ),
         operation_summary="",
         operation_description="",
     )
+
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
-    
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
