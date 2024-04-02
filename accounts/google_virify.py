1eec30e0084805f3685f404b3496549021a4d6d0
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-02 16:33:40 +0545

    clean db

--- accounts/google_virify.py
+++ accounts/google_virify.py
@@ -5,7 +5,9 @@ from google.oauth2 import id_token
 from django.conf import settings
 
 # GOOGLE_CLIENT_ID = '976671591908-6mbvs6crsd63pd571781sihul4qc40hi.apps.googleusercontent.com'
-GOOGLE_CLIENT_ID = '976671591908-sugqc0jsn1e4fasbhmnoe4rmcvk1665l.apps.googleusercontent.com'
+# GOOGLE_CLIENT_ID = '976671591908-sugqc0jsn1e4fasbhmnoe4rmcvk1665l.apps.googleusercontent.com'
+GOOGLE_CLIENT_ID = '976671591908-c496ffugo7650vm3k8kegrv4sd6rg34t.apps.googleusercontent.com'
+
 
 def VerifyGoogleToken(token):
     try:
