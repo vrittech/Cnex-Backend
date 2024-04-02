9b1cd78147e1de9c73855bac6037cfe0b680588d
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-02 12:38:19 +0545

    login with google

--- accounts/google_virify.py
+++ accounts/google_virify.py
@@ -4,7 +4,8 @@ from google.auth.transport import requests
 from google.oauth2 import id_token
 from django.conf import settings
 
-GOOGLE_CLIENT_ID = '976671591908-6mbvs6crsd63pd571781sihul4qc40hi.apps.googleusercontent.com'
+# GOOGLE_CLIENT_ID = '976671591908-6mbvs6crsd63pd571781sihul4qc40hi.apps.googleusercontent.com'
+GOOGLE_CLIENT_ID = '976671591908-sugqc0jsn1e4fasbhmnoe4rmcvk1665l.apps.googleusercontent.com'
 
 def VerifyGoogleToken(token):
     try:
