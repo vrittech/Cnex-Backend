6489e516cfbebc71d823e059a41a574fc4c4e663
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-05 17:54:59 +0545

    in admin  privacypolicy , help and support and term and condition

--- compaign/admin.py
+++ compaign/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Faqs,Banner,PrivacyPolicy
+from .models import Faqs,Banner,PrivacyPolicy,HelpAndSupport,TermAndCondition
 
 # Register your models here.
-admin.site.register([Faqs,Banner,PrivacyPolicy])
+admin.site.register([Faqs,Banner,PrivacyPolicy,HelpAndSupport,TermAndCondition])
