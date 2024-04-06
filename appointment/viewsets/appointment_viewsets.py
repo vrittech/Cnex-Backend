4cb03e684e8a19fcfe19c4bc18fb9a46703fbf88
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-06 17:28:40 +0545

    .

--- appointment/viewsets/appointment_viewsets.py
+++ appointment/viewsets/appointment_viewsets.py
@@ -20,6 +20,6 @@ class AppointmentViewsets(viewsets.ModelViewSet):
         if self.request.user.role in [roles.ADMIN,roles.SUPER_ADMIN]:
             return super().get_queryset()
         else:
-            return super().get_queryset().filter(user = self.request.user.id)
+            return super().get_queryset().filter(user_id = self.request.user.id)
     
     
