c0e53289df6d021c937a8e149017a226ad9cfe62
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-06 17:27:10 +0545

    user authentication

--- appointment/viewsets/appointment_viewsets.py
+++ appointment/viewsets/appointment_viewsets.py
@@ -1,11 +1,13 @@
 from ..models import Appointment
 from ..serializers.appointment_serializer import AppointmentReadSerializers,AppointmentWriteSerializers
 from ..utilities.importbase import *
+from rest_framework.permissions import  IsAuthenticated
+from accounts import roles
 
 class AppointmentViewsets(viewsets.ModelViewSet):
     serializer_class = AppointmentReadSerializers
-    # permission_classes = [AdminViewSetsPermission]
-    # authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
     pagination_class = MyPageNumberPagination
     queryset  = Appointment.objects.all()
 
@@ -13,5 +15,11 @@ class AppointmentViewsets(viewsets.ModelViewSet):
         if self.action in ['create','update','partial_update']:
             return AppointmentWriteSerializers
         return super().get_serializer_class()
+    
+    def get_queryset(self):
+        if self.request.user.role in [roles.ADMIN,roles.SUPER_ADMIN]:
+            return super().get_queryset()
+        else:
+            return super().get_queryset().filter(user = self.request.user.id)
     
     
