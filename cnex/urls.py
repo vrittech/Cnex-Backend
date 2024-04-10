1ef99b33ee9d578858a57518eeb20eeb10555025
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-10 10:13:28 +0545

    hide swagger

--- cnex/urls.py
+++ cnex/urls.py
@@ -59,6 +59,6 @@ urlpatterns = [
     
 
     # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
