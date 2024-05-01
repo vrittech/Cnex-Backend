c54a8c244471df7db439c1bff8b332711ae939bf
vrittech-manoj <manoj@vrittechnologies.com>
2024-05-01 10:41:47 +0545

    coupon viewset

--- coupon/viewsets/coupon_viewsets.py
+++ coupon/viewsets/coupon_viewsets.py
@@ -7,7 +7,7 @@ class CouponViewsets(viewsets.ModelViewSet):
     permission_classes = [AdminViewSetsPermission]
     authentication_classes = [JWTAuthentication]
     pagination_class = MyPageNumberPagination
-    queryset  = Coupon.objects.all()
+    queryset  = Coupon.objects.all().order_by('-created_date')
 
     def get_serializer_class(self):
         if self.action in ['create','update','partial_update']:
