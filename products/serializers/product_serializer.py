266f50938520efcfea79cd3bc1cf9416ec4a01c5
vrittech-manoj <manoj@vrittechnologies.com>
2024-05-30 23:54:27 +0545

    .

--- products/serializers/product_serializer.py
+++ products/serializers/product_serializer.py
@@ -281,6 +281,7 @@ def createProductDetailAfterVariation(variation_data,product,create_update):
         ProductDetailAfterVariation.objects.filter(product_id = product).delete() #this let variation delete from product. this is not best method. please use another best 
         
         for variation in variation_data:
+            
             ProductDetailAfterVariation_objs_list.remove(variation.get('id'))
 
             create_payload = {**variation,'variation_options':variation.get('id') ,'product': product} 
